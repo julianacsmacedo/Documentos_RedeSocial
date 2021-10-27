1a54a2292bdaf6974afdfdca6e5e254547909d30
julianacsmacedo <julianamacedo2008@gmail.com>
2021-10-27 13:38:55 -0300

    atualizando atributo usuario

--- RedeSocial.sql
+++ RedeSocial.sql
@@ -1,33 +1,33 @@
 CREATE TABLE `Temas` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`categoria` varchar(255) NOT NULL,
-	`descrição` varchar(255) NOT NULL,
+	`descricao` varchar(255) NOT NULL,
 	`valido` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `postagens` (
+CREATE TABLE `Postagens` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`titulo` varchar(255) NOT NULL,
-	`texto` varchar(255) NOT NULL,
+	`texto` varchar(1000) NOT NULL,
 	`data` DATETIME NOT NULL,
 	`localizacao` varchar(255) NOT NULL,
-	`temas_id` bigint(255) NOT NULL,
-	`usuario_id` varchar(255) NOT NULL,
+	`temas_id` bigint NOT NULL,
+	`usuario_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `usuario` (
+CREATE TABLE `Usuario` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
-	`nome` bigint NOT NULL,
-	`email` varchar(255) NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`usuario` varchar(255) NOT NULL,
 	`senha` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `postagens` ADD CONSTRAINT `postagens_fk0` FOREIGN KEY (`temas_id`) REFERENCES `Temas`(`id`);
+ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk0` FOREIGN KEY (`temas_id`) REFERENCES `Temas`(`id`);
 
-ALTER TABLE `postagens` ADD CONSTRAINT `postagens_fk1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`id`);
+ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk1` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario`(`id`);
 
 
 
